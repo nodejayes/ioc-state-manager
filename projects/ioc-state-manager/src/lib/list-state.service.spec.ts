aef6b6fd9c3922bc85d800f54228209cae866431
Markus Gilg <markusgilg@outlook.de>
2019-08-01 21:26:47 +0200

    list state service
    test load

--- projects/ioc-state-manager/src/lib/list-state.service.spec.ts
+++ projects/ioc-state-manager/src/lib/list-state.service.spec.ts
@@ -13,14 +13,29 @@ describe('ListStateService', () => {
     });
 
     it('mutations was register', () => {
-        expect(TestStateService.loadMutation).toBeDefined();
         expect(TestStateService.loadMutation).toEqual(TestMutations.Load);
+        expect(TestStateService.loadSuccessMutation).toEqual(TestMutations.LoadSuccess);
+        expect(TestStateService.loadFailMutation).toEqual(TestMutations.LoadFail);
     });
 
-    it('mutation can calling', () => {
+    it('set loading state', () => {
         service.load();
         expect(service.Loading).toBeTruthy();
         expect(service.Loaded).toBeFalsy();
         expect(service.Data).toEqual([]);
+    });
+
+    it('emit OnMutations and see result', () => {
+        service.data$.subscribe((d) => {
+            if (d && d.length === 3) {
+                expect(service.Loading).toBeFalsy();
+                expect(service.Loaded).toBeTruthy();
+                expect(service.Data.length).toEqual(3);
+                expect(service.Data[0].id).toEqual(1);
+                expect(service.Data[1].id).toEqual(2);
+                expect(service.Data[2].id).toEqual(3);
+            }
+        });
+        service.load();
     });
 });
--- projects/ioc-state-manager/src/lib/test_cases/test-list-state.service.ts
+++ projects/ioc-state-manager/src/lib/test_cases/test-list-state.service.ts
@@ -1,5 +1,6 @@
 import {Injectable} from '@angular/core';
 import {IDetectable, IListState, ListStateService} from '../list-state.service';
+import {OnMutation} from '../side-effects.decorator';
 
 export enum TestStateNames {
     TEST = 'test',
@@ -16,11 +17,15 @@ export interface ITestState extends IListState<IUser> {
 
 export enum TestMutations {
     Load = '[test] load',
+    LoadSuccess = '[test] load success',
+    LoadFail = '[test] load fail',
 }
 
 @Injectable()
 export class TestStateService extends ListStateService<ITestState, IUser> {
     static loadMutation = TestMutations.Load;
+    static loadSuccessMutation = TestMutations.LoadSuccess;
+    static loadFailMutation = TestMutations.LoadFail;
 
     constructor() {
         super(TestStateNames.TEST, {
@@ -30,5 +35,21 @@ export class TestStateService extends ListStateService<ITestState, IUser> {
             error_msg: null,
             version: 0,
         });
+    }
+
+    @OnMutation(TestMutations.Load)
+    private onLoad() {
+        // simulate Server Request
+        setTimeout(() => {
+            this.loadSuccess([
+                {id: 1, name: 'Udo'},
+                {id: 2, name: 'Paul'},
+                {id: 3, name: 'Martina'},
+            ]);
+        }, 1000);
+    }
+
+    testErrorLoad() {
+        this.loadFail('error_message');
     }
 }
