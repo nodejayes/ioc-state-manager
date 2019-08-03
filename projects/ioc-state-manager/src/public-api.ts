417f5e151844a10f339771ffdcd2bf43bdef5d9c
Markus Gilg <markusgilg@outlook.de>
2019-08-03 12:21:19 +0200

    release 1.1.2
    add missing exports

--- projects/ioc-state-manager/src/public-api.ts
+++ projects/ioc-state-manager/src/public-api.ts
@@ -6,3 +6,4 @@ export {IMutation}  from './lib/ioc-store.service';
 export {Mutation}   from './lib/mutation.decorator';
 export {OnMutation} from './lib/side-effects.decorator';
 export {IocService} from './lib/ioc-service';
+export {ListStateService, IListState, IDetectable, ILoadableState} from './lib/list-state.service.js';
